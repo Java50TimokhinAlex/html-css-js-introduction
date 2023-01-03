b644437ecb7636628971efd09fca466da5a50123
Alex <alexthuder87@gmail.com>
2023-01-03 18:06:41 +0200

    HW 13 beggining arrays

--- app.js
+++ app.js
@@ -1,68 +1,10 @@
+// const ar = [];
+// const ar1 = [1,2,3,4]
+// ar.push(...ar1); //spread  потрошит  массив на элементы
+// ar.push('abc');
 
-
-// let num = 12345;
-// let str = String(num);
-// let sum1 = 0;
-// for (let digit of str)
-// {
-//   sum1 += Number(digit);
-// }
-// console.log(sum1);
-
-
-
-function getDigitsSum(number) {
-   
-    let sum = 0;
-
-    while(number > 0) {
-        sum += number % 10;
-        number = number / 10;
-        number = Math.floor(number);
-    }
-    return sum;
+function getRandomNumber(min, max) {
+  min = Math.trunc(min);
+  max = Math.trunc(max);
+  return min + Math.random() * (max - min);
 }
-console.log(getDigitsSum(1234))
-
-
-
-
-let res = "A" + "AS" / 1 + "AS";
-let str1 = res;
-str1 = str1.toLowerCase();
-console.log(str1);
-
-
-
-function getDigitsRev(n) {
-  let rev = "";
-  if (n < 0) {
-      rev += "-";
-      n *= -1;
-  }
-  let number = Math.floor(n);
- 
-
-  while(number > 0) {
-      rev += number % 10;
-      number = number / 10;
-      number = Math.floor(number);
-  }
-  return rev;
-}
-console.log(getDigitsRev(-987654321))
-
-
-
-function computeExpression(expressionStr) {
-  return console.log(eval(expressionStr));
-}
-console.log(eval(9000 / ((10 + 20) ** 2)))
-
-
-
-
-
-
-
-
