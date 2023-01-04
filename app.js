ee78cb8d308a702da92a0720385a8f07e45b4e85
Alex <alexthuder87@gmail.com>
2023-01-05 00:57:38 +0200

    HW_#14 arrays: functions map and filter

--- app.js
+++ app.js
@@ -1,52 +1,38 @@
-const array =  [10, 20, 30, -10, 11, 100];
-array.splice(2, 2);
-console.log(array.join("."))
-
- const index = array.indexOf(30);
-
-//  delete 
-// (removing all numbers from 30)
-const res = array.splice(index, 10)
-array.splice(index, array.length, -1000);
-
-// insert
-array.splice(index, 0, -10, -20);
-
-// replace
-array.splice(index, 2, 20);
-const ar11 = [40, 200];
-array.splice(index, 1, ...ar11)
-
-const numbersPerPage = 2;
-const page = 2;
-const firstIndex = (page - 1) * numbersPerPage;
-const lastIndex = firstIndex + numbersPerPage;
-const pageNumbers = array.slice(firstIndex, lastIndex)
-
-// __________________________________________________
-
-const ar1 = array.map(multiply2);
-function multiply2(numbers) {
-  return numbers * 2;
-}
-
-const ar2 = array.map(function(number, index)
+function coloringString(str1, str2)
 {
-  return index + 1 + ". " + number;
+    let ar1 = Array.from(str1);
+    let ar2 = Array.from(str2);
+    if (ar1.length!=ar2.length) {
+      console.log("Error");
+    }
+    const res = ar2.map(function (number, index)
+    {
+        if  (ar2[index] === ar1[index]) {
+          return"green";
+        }
+        else if (ar1.indexOf(number) != index && ar1.indexOf(number) > 0 ) {
+          return "yellow";
+        }
+        else  {return "red";}
+    })
+    return res;
+}
+console.log(coloringString ("pappy", "apple"))
+
+// ____________________________________________________________________________
+
+
+function getNumbersWithDigitsAmount(digitsAmount, array)
+{
+let numbersOfDigits = digitsAmount;
+const res = array.filter(function (number)
+{
+    if (number < 0) {
+      number = number * -1;}
+      number = String(number);
+    if (number.length === numbersOfDigits)
+    return +(number);
 });
-
-const ar3 = array.map(function(number) {
-  return `<li>${number}</li>`
-})
-const ulElement = `<ul>${ar3.join('')}</ul>`
-
-// __________________________________________________
-const min = 2;
-const max = 15;
-const ar4 = array.filter(function(number) {
-  return number >= min && number <= max;
-})
-
-const ar5 = array.filter(function(__, index) {
-  return index % 2 === 0;
-})
+return res;
+}
+console.log(getNumbersWithDigitsAmount(3, [5,100,-100,25,1000,-32,22,16,-17,250, -666]))
