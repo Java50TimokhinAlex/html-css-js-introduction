b0d49c9dae0a5c6d32f40df921beefadf8c37a9c
Alex <alexthuder87@gmail.com>
2023-02-03 12:24:52 +0200

    CW #22 add changes into employees application

---
+++ src/config/employee-config.js
@@ -0,0 +1,13 @@
+export const employeeConfig = {
+  minId: 100000000,
+  maxId: 999999999,
+  minSalary: 5000,
+  maxSalary: 50000,
+  minYear: 1970,
+  maxYear: 2001,
+  countries: {
+    "Israel": ["Lod", "Tel Aviv", "Rehovot"],
+    "USA": ["New York", "Boston", "Washington"], 
+    
+  }
+}
