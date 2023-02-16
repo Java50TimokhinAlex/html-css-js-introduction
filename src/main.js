ebdf3ca119d2f5ded8704b71928afc16cc364877
Alex <alexthuder87@gmail.com>
2023-02-16 17:29:51 +0200

    video_player try\catch

---
+++ src/main.js
@@ -0,0 +1,19 @@
+import { sleep } from "./utilits/sleep.js";
+import { VideoPlayer } from "./ui/video-player.js";
+import { DataForm } from "./ui/input-data-form.js";
+import { checkPlayingTime } from "./service/checkTime.js";
+const dataForm = new DataForm("form-section");
+const videoPlayer = new VideoPlayer("video-section");
+dataForm.addHandler(async (data) => {
+
+    let res = checkPlayingTime(data.time)
+    if (!res) {
+        videoPlayer.setUrl(data.link);
+        videoPlayer.start();
+        await sleep(data.time * 1000);
+        videoPlayer.stop();
+    }
+    return res;
+
+})
+
